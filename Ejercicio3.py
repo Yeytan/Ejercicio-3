9c744b673c1701459f336d24f601ce5db79ad178
Yeytan <esdras.yeytan@gmail.com>
2023-03-20 00:39:53 -0600

    actualizar

--- Ejercicio3.py
+++ Ejercicio3.py
@@ -4,77 +4,82 @@ from tkinter import ttk
 import tkinter as ttk
 
 raiz = Tk()
-raiz.geometry("1250x650")
+raiz.geometry("1500x650")
 raiz.configure(background="black")
 
-MenuFrame = ttk.Frame(raiz,background="turquoise4")
-MenuFrame.grid(column=0,row=0, sticky=(W))
+#Frame Principal
 
-FrameP = ttk.Frame(raiz, background="black")
-FrameP.grid(column=0, row=1, sticky=(W), pady=10)
+mainFrame = ttk.Frame(raiz,background="turquoise4")
+mainFrame.grid(column=0,row=0, sticky=(W))
 
-#frames izquierda arriba
+#Frame base negro
 
-frameIzquierda = ttk.Frame(FrameP,background="black")
-frameIzquierda.grid(column=0,row=0,pady=10,sticky=(W,N))
+mainframeblack = ttk.Frame(raiz, background="black")
+mainframeblack.grid(column=0, row=1, sticky=(W), pady=10)
 
-Frame1 = ttk.Frame(frameIzquierda,background="black")
-Frame1.grid(column=0,row=0,sticky=(W))
+#Frame izquierdo arriba
 
-IzqFrame = ttk.Frame(Frame1,background="#333333")
-IzqFrame.grid(column=0,row=0,sticky=(W),padx=5)
+mainFrameizq = ttk.Frame(mainframeblack,background="black")
+mainFrameizq.grid(column=0,row=0,pady=10,sticky=(W,N))
 
-DerFrame = ttk.Frame(Frame1,background="#333333")
-DerFrame.grid(column=1,row=0,sticky=(W,N))
+mainFrameizq2 = ttk.Frame(mainFrameizq,background="black")
+mainFrameizq2.grid(column=0,row=0,sticky=(W))
 
-#framederecha
+#Izquierdo
+IzqFrame = ttk.Frame(mainFrameizq2,background="#333333")
+IzqFrame.grid(column=0,row=0,sticky=(W),padx=120)
 
-framederecha = ttk.Frame(FrameP,background="#333333")
-framederecha.grid(column=1,row=0,sticky=(W,N),padx=5)
+#Derecho
+DerFrame = ttk.Frame(mainFrameizq2,background="#333333")
+DerFrame.grid(column=0,row=0,sticky=(W,N), padx=370)
 
-frame3 = ttk.Frame(framederecha,background="#333333")
-frame3.grid(column=0,row=0, sticky=(W,N))
+#Frame derecho arriba
+mainFrameder = ttk.Frame(mainframeblack,background="#333333")
+mainFrameder.grid(column=0,row=0,sticky=(W,N),padx=790, pady=10)
 
-produccionFrame = ttk.Frame(frame3,background="#333333")
-produccionFrame.grid(column=2,row=0,sticky=(W,N))
+mainFrameder2 = ttk.Frame(mainFrameder,background="#333333")
+mainFrameder2.grid(column=0,row=0, sticky=(W,N))
+
+mainFrameder3= ttk.Frame(mainFrameder2,background="#333333")
+mainFrameder3.grid(column=0,row=0,sticky=(W,N))
 
 #frame izquierda 
 
-Frame2 = ttk.Frame(frameIzquierda,background="black")
+Frame2 = ttk.Frame(mainFrameizq,background="black", padx=118)
 Frame2.grid(column=0,row=1,sticky=(W,N))
 
 DerAbajoFrame = ttk.Frame(Frame2,background="#333333" )
-DerAbajoFrame.grid(column=0,row=0,sticky=(W,N),padx=5,pady=10 )
+DerAbajoFrame.grid(column=0,row=0,sticky=(W,N),padx=1,pady=5 )
 
 DerAbajoFrame1 = ttk.Frame(Frame2,background="#333333")
-DerAbajoFrame1.grid(column=1,row=0,sticky=(W),padx=5, pady=10)
+DerAbajoFrame1.grid(column=1,row=0,sticky=(W), padx=2, pady=5)
 
-ttk.Label(MenuFrame,text="SPAI 4.0",font=("verdana",18,"bold"),foreground="white",background="turquoise4", width=200,anchor="w",height=2).grid(column=1,row=0,sticky=(W))
+#Titulo
+
+ttk.Label(mainFrame,text="SPAI 4.0",font=("verdana",11,"bold"),foreground="white",background="turquoise4", width=200,anchor="w",height=2).grid(column=1,row=0,sticky=(W))
+
+imagen3 = PhotoImage(file="boton.png")
+imagenb = ttk.Button(mainFrame,background="turquoise4",relief="flat")
+imagenb.grid(column=0,row=0,sticky=(W))
+imagenb['image'] = imagen3
+
+#Texto Indicadores
 
 ttk.Label(IzqFrame,text="Indicadores Digitales",font=("verdana",13,"bold"),foreground="turquoise4",background="#333333", width=20).grid(column=0,row=0,sticky=(W))
-ttk.Label(IzqFrame,text="Linea 1:",font=("verdana",13,"bold"),foreground="white",background="#333333").grid(column=0,row=1,sticky=(W),pady=11)
-ttk.Label(IzqFrame,text="Linea 2:",font=("verdana",13,"bold"),foreground="white",background="#333333").grid(column=0,row=2,sticky=(W),pady=11)
-ttk.Label(IzqFrame,text="Linea 1:",font=("verdana",13,"bold"),foreground="white",background="#333333").grid(column=0,row=3,sticky=(W),pady=11)
-ttk.Label(IzqFrame,text="Gabinete:",font=("verdana",13,"bold"),foreground="white",background="#333333").grid(column=0,row=4,sticky=(W),pady=11)
-ttk.Label(IzqFrame,text="Alarma:",font=("verdana",13,"bold"),foreground="white",background="#333333").grid(column=0,row=5,sticky=(W),pady=11)
-ttk.Label(IzqFrame,text="Alarma 2:",font=("verdana",13,"bold"),foreground="white",background="#333333").grid(column=0,row=6,sticky=(W),pady=11)
+ttk.Label(IzqFrame,text="Linea 1        On",font=("verdana",13,"bold"),foreground="white",background="#333333").grid(column=0,row=1,sticky=(W),pady=11)
+ttk.Label(IzqFrame,text="Linea 2        On",font=("verdana",13,"bold"),foreground="white",background="#333333").grid(column=0,row=2,sticky=(W),pady=11)
+ttk.Label(IzqFrame,text="Linea 1:       On",font=("verdana",13,"bold"),foreground="white",background="#333333").grid(column=0,row=3,sticky=(W),pady=11)
+ttk.Label(IzqFrame,text="Gabinete: Abierto",font=("verdana",13,"bold"),foreground="white",background="#333333").grid(column=0,row=4,sticky=(W),pady=11)
+ttk.Label(IzqFrame,text="Alarma:        On",font=("verdana",13,"bold"),foreground="white",background="#333333").grid(column=0,row=5,sticky=(W),pady=11)
+ttk.Label(IzqFrame,text="Alarma 2:     Off",font=("verdana",13,"bold"),foreground="white",background="#333333").grid(column=0,row=6,sticky=(W),pady=11)
+
+#Temperatura
 
 ttk.Label(DerFrame,text="Temperatura",font=("verdana",13,"bold"),foreground="turquoise4",background="#333333").grid(column=0,row=0,sticky=(W))
 ttk.Label(DerFrame,text="Temperatura 1:",font=("verdana",13,"bold"),foreground="white",background="#333333").grid(column=0,row=1,sticky=(W),pady=30,padx=15)
 ttk.Label(DerFrame,text="Temperatura 2:",font=("verdana",13,"bold"),foreground="white",background="#333333").grid(column=1,row=1,sticky=(W),pady=30)
 ttk.Label(DerFrame,text="Temp.Agua: 58 °C",font=("verdana",13,"bold"),foreground="white",background="#333333").grid(column=0,row=3,sticky=(N,S),columnspan=2, pady=2)
 ttk.Label(DerFrame,text="Temp.Ambiente: 32 °C",font=("verdana",13,"bold"),foreground="white",background="#333333").grid(column=0,row=4,sticky=(N,S),columnspan=2,pady=10)
-
-ttk.Label(produccionFrame,text="Produccion",font=("verdana",13,"bold"),foreground="turquoise4",background="#333333").grid(column=0,row=0,sticky=(W),padx=30)
-
-ttk.Label(DerAbajoFrame,text="Velocidad Bomba:",font=("verdana",13,"bold"),foreground="white",background="#333333").grid(column=0,row=0,sticky=(W), pady=25, padx=55)
-
-ttk.Label(DerAbajoFrame1,text="Nivel del Tanque",font=("verdana",13,"bold"),foreground="turquoise4",background="#333333").grid(column=0,row=0,sticky=(W), padx= 15)
-
-imagen3 = PhotoImage(file="boton.png")
-imagenb = ttk.Button(MenuFrame,background="turquoise4",relief="flat")
-imagenb.grid(column=0,row=0,sticky=(W))
-imagenb['image'] = imagen3
 
 imagen5 = PhotoImage(file="Temperatura1.png")
 imagenT1 = ttk.Button(DerFrame,background="#333333",relief="flat")
@@ -86,15 +91,28 @@ imagenT2 = ttk.Button(DerFrame,background="#333333",relief="flat")
 imagenT2.grid(column=1,row=2,sticky=(W))
 imagenT2['image'] = imagen6
 
+#Produccion
+
+ttk.Label(mainFrameder3,text="Produccion",font=("verdana",13,"bold"),foreground="turquoise4",background="#333333").grid(column=0,row=0,sticky=(W),padx=30)
+
 imagen7 = PhotoImage(file="Grafica.png")
-imagenG = ttk.Button(produccionFrame,background="#333333",relief="flat")
+imagenG = ttk.Button(mainFrameder3,background="#333333",relief="flat")
 imagenG.grid(column=0,row=1,sticky=(W))
 imagenG['image'] = imagen7
+
+
+#Velocidad
+
+ttk.Label(DerAbajoFrame,text="Velocidad Bomba:",font=("verdana",13,"bold"),foreground="white",background="#333333").grid(column=0,row=0,sticky=(W), pady=25, padx=55)
 
 imagen8 = PhotoImage(file="Velocidad.png")
 imagen4 = ttk.Button(DerAbajoFrame,background="#333333",relief="flat")
 imagen4.grid(column=0,row=1,sticky=(W))
 imagen4['image'] = imagen8
+
+#Tanque
+
+ttk.Label(DerAbajoFrame1,text="Nivel del Tanque                          ",font=("verdana",13,"bold"),foreground="turquoise4",background="#333333").grid(column=0,row=0,sticky=(W), padx= 15)
 
 imagen9 = PhotoImage(file="Tanque.png")
 imagenTan = ttk.Button(DerAbajoFrame1,background="#333333",relief="flat")
