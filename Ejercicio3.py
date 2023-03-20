a74f764ac4fa1a67b6a0e6a6b3cdc0fa4b5860b0
Yeytan <esdras.yeytan@gmail.com>
2023-03-20 11:22:36 -0600

    actualizar

--- Ejercicio3.py
+++ Ejercicio3.py
@@ -42,26 +42,24 @@ mainFrameder2.grid(column=0,row=0, sticky=(W,N))
 
 mainFrameder3= ttk.Frame(mainFrameder2,background="#333333")
 mainFrameder3.grid(column=0,row=0,sticky=(W,N))
+ #Frameizqabajo
+mainFrameizqabajo = ttk.Frame(mainFrameizq,background="black", padx=118)
+mainFrameizqabajo.grid(column=0,row=1,sticky=(W,N))
 
-#frame izquierda 
+mainFrameizqabajo1 = ttk.Frame(mainFrameizqabajo,background="#333333" )
+mainFrameizqabajo1.grid(column=0,row=0,sticky=(W,N),padx=1,pady=5 )
 
-Frame2 = ttk.Frame(mainFrameizq,background="black", padx=118)
-Frame2.grid(column=0,row=1,sticky=(W,N))
-
-DerAbajoFrame = ttk.Frame(Frame2,background="#333333" )
-DerAbajoFrame.grid(column=0,row=0,sticky=(W,N),padx=1,pady=5 )
-
-DerAbajoFrame1 = ttk.Frame(Frame2,background="#333333")
-DerAbajoFrame1.grid(column=1,row=0,sticky=(W), padx=2, pady=5)
+mainFrameizqabajo2 = ttk.Frame(mainFrameizqabajo,background="#333333")
+mainFrameizqabajo2.grid(column=1,row=0,sticky=(W), padx=2, pady=5)
 
 #Titulo
 
 ttk.Label(mainFrame,text="SPAI 4.0",font=("verdana",11,"bold"),foreground="white",background="turquoise4", width=200,anchor="w",height=2).grid(column=1,row=0,sticky=(W))
 
-imagen3 = PhotoImage(file="boton.png")
+imagenboton = PhotoImage(file="boton.png")
 imagenb = ttk.Button(mainFrame,background="turquoise4",relief="flat")
 imagenb.grid(column=0,row=0,sticky=(W))
-imagenb['image'] = imagen3
+imagenb['image'] = imagenboton
 
 #Texto Indicadores
 
@@ -73,6 +71,39 @@ ttk.Label(IzqFrame,text="Gabinete: Abierto",font=("verdana",13,"bold"),foregroun
 ttk.Label(IzqFrame,text="Alarma:        On",font=("verdana",13,"bold"),foreground="white",background="#333333").grid(column=0,row=5,sticky=(W),pady=11)
 ttk.Label(IzqFrame,text="Alarma 2:     Off",font=("verdana",13,"bold"),foreground="white",background="#333333").grid(column=0,row=6,sticky=(W),pady=11)
 
+imagenverde1 = PhotoImage(file="botonverde2.png")
+imagenT1 = ttk.Button(IzqFrame,background="#333333",relief="flat")
+imagenT1.grid(column=0,row=1, sticky=(E))
+imagenT1['image'] = imagenverde1
+
+imagenverde2 = PhotoImage(file="botonverde2.png")
+imagenT1 = ttk.Button(IzqFrame,background="#333333",relief="flat")
+imagenT1.grid(column=0,row=2, sticky=(E))
+imagenT1['image'] = imagenverde2
+
+imagenverde3 = PhotoImage(file="botonverde2.png")
+imagenT1 = ttk.Button(IzqFrame,background="#333333",relief="flat")
+imagenT1.grid(column=0,row=3, sticky=(E))
+imagenT1['image'] = imagenverde3
+
+imagenroja = PhotoImage(file="botonrojo2.png")
+imagenT1 = ttk.Button(IzqFrame,background="#333333",relief="flat")
+imagenT1.grid(column=0,row=4, sticky=(E))
+imagenT1['image'] = imagenroja
+
+imagenroja1 = PhotoImage(file="botonrojo2.png")
+imagenT1 = ttk.Button(IzqFrame,background="#333333",relief="flat")
+imagenT1.grid(column=0,row=5, sticky=(E))
+imagenT1['image'] = imagenroja1
+
+imagenverde4 = PhotoImage(file="botonverde2.png")
+imagenT1 = ttk.Button(IzqFrame,background="#333333",relief="flat")
+imagenT1.grid(column=0,row=6, sticky=(E))
+imagenT1['image'] = imagenverde4
+
+
+
+
 #Temperatura
 
 ttk.Label(DerFrame,text="Temperatura",font=("verdana",13,"bold"),foreground="turquoise4",background="#333333").grid(column=0,row=0,sticky=(W))
@@ -81,42 +112,42 @@ ttk.Label(DerFrame,text="Temperatura 2:",font=("verdana",13,"bold"),foreground="
 ttk.Label(DerFrame,text="Temp.Agua: 58 °C",font=("verdana",13,"bold"),foreground="white",background="#333333").grid(column=0,row=3,sticky=(N,S),columnspan=2, pady=2)
 ttk.Label(DerFrame,text="Temp.Ambiente: 32 °C",font=("verdana",13,"bold"),foreground="white",background="#333333").grid(column=0,row=4,sticky=(N,S),columnspan=2,pady=10)
 
-imagen5 = PhotoImage(file="Temperatura1.png")
+imagentemp = PhotoImage(file="Temperatura1.png")
 imagenT1 = ttk.Button(DerFrame,background="#333333",relief="flat")
 imagenT1.grid(column=0,row=2,sticky=(W))
-imagenT1['image'] = imagen5
+imagenT1['image'] = imagentemp
 
-imagen6 = PhotoImage(file="Temperatura2.png")
+imagentemp2 = PhotoImage(file="Temperatura2.png")
 imagenT2 = ttk.Button(DerFrame,background="#333333",relief="flat")
 imagenT2.grid(column=1,row=2,sticky=(W))
-imagenT2['image'] = imagen6
+imagenT2['image'] = imagentemp2
 
 #Produccion
 
 ttk.Label(mainFrameder3,text="Produccion",font=("verdana",13,"bold"),foreground="turquoise4",background="#333333").grid(column=0,row=0,sticky=(W),padx=30)
 
-imagen7 = PhotoImage(file="Grafica.png")
+imagengrafica = PhotoImage(file="Grafica.png")
 imagenG = ttk.Button(mainFrameder3,background="#333333",relief="flat")
 imagenG.grid(column=0,row=1,sticky=(W))
-imagenG['image'] = imagen7
+imagenG['image'] = imagengrafica
 
 
 #Velocidad
 
-ttk.Label(DerAbajoFrame,text="Velocidad Bomba:",font=("verdana",13,"bold"),foreground="white",background="#333333").grid(column=0,row=0,sticky=(W), pady=25, padx=55)
+ttk.Label(mainFrameizqabajo1,text="Velocidad Bomba:",font=("verdana",13,"bold"),foreground="white",background="#333333").grid(column=0,row=0,sticky=(W), pady=25, padx=55)
 
-imagen8 = PhotoImage(file="Velocidad.png")
-imagen4 = ttk.Button(DerAbajoFrame,background="#333333",relief="flat")
+imagenvel = PhotoImage(file="Velocidad.png")
+imagen4 = ttk.Button(mainFrameizqabajo1,background="#333333",relief="flat")
 imagen4.grid(column=0,row=1,sticky=(W))
-imagen4['image'] = imagen8
+imagen4['image'] = imagenvel
 
 #Tanque
 
-ttk.Label(DerAbajoFrame1,text="Nivel del Tanque                          ",font=("verdana",13,"bold"),foreground="turquoise4",background="#333333").grid(column=0,row=0,sticky=(W), padx= 15)
+ttk.Label(mainFrameizqabajo2,text="Nivel del Tanque                          ",font=("verdana",13,"bold"),foreground="turquoise4",background="#333333").grid(column=0,row=0,sticky=(W), padx= 15)
 
-imagen9 = PhotoImage(file="Tanque.png")
-imagenTan = ttk.Button(DerAbajoFrame1,background="#333333",relief="flat")
+imagentanque = PhotoImage(file="Tanque.png")
+imagenTan = ttk.Button(mainFrameizqabajo2,background="#333333",relief="flat")
 imagenTan.grid(column=0,row=1,sticky=(W))
-imagenTan['image'] = imagen9
+imagenTan['image'] = imagentanque
 
 raiz.mainloop()
